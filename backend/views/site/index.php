1430b525dd64803071c038076646d312f5190340
Crox Sánchez <crox.sanchez@gmail.com>
2016-04-24 23:51:52 -0430

    CRUDS del backend para User, UserType, Role, Status y Profile.

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -2,14 +2,14 @@
 
 /* @var $this yii\web\View */
 
-$this->title = 'My Yii Application';
+$this->title = 'BcauseNet';
 ?>
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
+        <h1>Bienvenido a BcauseNet!</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <p class="lead">Ha ingresado exitosamente al backend de usuarios autorizados.</p>
 
         <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
     </div>
