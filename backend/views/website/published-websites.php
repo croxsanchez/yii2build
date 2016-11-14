77e1c593f0a0c82aeba50cde1f79437f59f2d0dd
Crox Sánchez <crox.sanchez@gmail.com>
2016-11-14 17:49:40 -0400

    Modiificaciones con Website, Domain y acciones.

---
+++ backend/views/website/published-websites.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel backend\models\customer\CustomerRecordSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'My Published Sites';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="customer-record-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+            'id',
+            [
+                'attribute' => 'name',
+                'label' => 'Domain Name',
+            ],
+            'customerName',
+            'paymentStatus',
+            [
+                'class' => 'yii\grid\ActionColumn',
+                'header' => 'Update Info',
+                'template' => '{update}',
+            ],
+        ],
+    ]); ?>
+</div>
